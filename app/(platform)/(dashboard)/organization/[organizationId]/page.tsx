37e12b908727996352df9be7dfdf78cb6f899cba
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2024-02-27 13:26:24 -0300

    faltando board-popover-form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,24 +1,18 @@
-import { auth } from "@clerk/nextjs";
-import { db } from "@/lib/db";
-import { Button } from "@/components/ui/button";
-import { Board } from "./board";
-import { Form } from "./form";
-
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 const OrganizationIdPage = async() =>{
-
-    const boards = await db.board.findMany();
-    await db.$disconnect();
     
     return(
-        <div>
-            <div className="space-y-2">
-                <Form />
-                {boards.map((board) =>(
-                    <Board key={board.id} id={board.id} title={board.title} />
-                ) )}
+        <div className="w-full mb-20">
+            <Info />
+            <Separator className="my-4" />
+            <div className="px-2 md:px-4">
+                <BoardList />
             </div>
         </div>
     );
 };
+
 
 export default OrganizationIdPage;
