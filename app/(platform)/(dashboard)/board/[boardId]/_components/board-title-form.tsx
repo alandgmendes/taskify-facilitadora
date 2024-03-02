ca54b41d7cfad93b78b1d2e8bb3951b89aeb5a3f
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2024-03-02 17:39:49 -0300

    board navbar 1 ready

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { FormInput } from "@/components/form/form-input";
+import { Button } from "@/components/ui/button";
+import { Board } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
+
+interface BoardTitleFormProps { 
+    data: Board;
+}
+export const BoardTitleForm = ({
+    data
+}: BoardTitleFormProps) => {
+    const formRef = useRef<ElementRef<"form">>(null);
+    const inputRef = useRef<ElementRef<"input">>(null);
+    const [isEditing, setIsEditing ] = useState(false);
+
+    const disableEditing = () =>{
+        setIsEditing(false);
+    }
+
+    const enableEditing= () =>{
+        setIsEditing(true);
+        setTimeout(()=>{
+            inputRef.current?.focus();
+            inputRef.current?.select();
+        })
+    }
+
+    const onSubmit = (formData: FormData) =>{
+        console.log('submit');
+        console.log(formData.get("title"));
+        debugger;
+    }
+
+    const onBlur = () => {
+        formRef.current?.requestSubmit();
+    }
+
+    if(isEditing) {
+        return (
+            <form action={onSubmit} ref={formRef} className="flex items-center gap-x-2">
+                <FormInput 
+                    id="title"
+                    onBlur={onBlur}
+                    defaultValue={data.title}
+                    ref={inputRef}
+                    className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+                >
+
+                </FormInput>
+            </form>
+        )
+    }
+
+    return (
+        <Button
+            variant="transparent"
+            className="font-bold text-lg h-auto w-auto p-1 px-2"
+            onClick={enableEditing}
+        >
+            {data.title}
+        </Button>
+    )
+}
---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,9 @@
+const BoardIdPage = () =>{
+    return (
+        <div>
+            Id board page
+        </div>
+    );
+}
+
+export default BoardIdPage;
